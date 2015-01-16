56f5c8f761f755ac0c60cf4e599a8be3b31cb176
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-01-16 14:13:09 +0100

    Clean-up.

--- R/fusedRidge.R
+++ R/fusedRidge.R
@@ -42,7 +42,7 @@ createS <- function(n, p) {
   # - Function that computes the value of the (negative) combined log-likelihood
   # - Slist > A list sample covariance matrices for each class
   # - Plist > A list of the same length as (Slist) of precision matrices
-  #  (possibly regularized inverse of covariance or correlation matrices)
+  #   (possibly regularized inverse of covariance or correlation matrices)
   # - n > A vector of sample sizes of the same length as Slist.
   ##############################################################################
 
@@ -51,56 +51,53 @@ createS <- function(n, p) {
 }
 
 
-.fusedUpdate <- function(k0, OList, SList, TList,
-                         ns, lambda1, lambda2) {
+.fusedUpdate <- function(k0, PList, SList, TList, ns, lambda1, lambda2) {
   ##############################################################################
   # - (Internal) Update the scatter matrices
   # - k0      > An integer giving the class estimate to be updated.
-  # - OList   > A list of matrices giving the current estimate (Omega).
-  # - SList   > A list of sample correlation matrices the same size as OList.
+  # - PList   > A list of matrices giving the current precision estimates.
+  # - SList   > A list of sample correlation matrices the same size as PList.
   # - TList   > A list of target matrices the same size as SList
   # - ns      > A vector giving the sample sizes.
   # - lambda1 > The ridge penalty (a postive number).
   # - labmda2 > The fused penalty (a non-negative number).
   ##############################################################################
 
-  # Modify sample covariance matrices
   b <- lambda2/ns[k0]                                    # Modified penalty2
-  OmT <- mapply(`-`, OList[-k0], TList[-k0])    # Omega minus Target
+  OmT <- mapply(`-`, PList[-k0], TList[-k0])             # Omega minus Target
   S0 <- SList[[k0]] - b*Reduce(`+`, OmT)                 # Modified S
-
-  # Modified lambda
-  a <- (lambda1 + 2*(length(SList) - 1)*lambda2)/ns[k0]  # Modified penalty2
+  a <- (lambda1 + 2*(length(SList) - 1)*lambda2)/ns[k0]  # Modified penalty1
   return(ridgeS(S0, lambda = a, target = TList[[k0]]))
 }
 
 
-fusedRidgeS <- function(SList, TList, ns, lambda1, lambda2,
+fusedRidgeS <- function(SList, ns, TList = lapply(SList, default.target),
+                        lambda1, lambda2,
                         max.ite = 100L, verbose = TRUE, eps = 1e-7) {
   ##############################################################################
   # - The fused ridge estimate for a given lambda1 and lambda2
   # - SList   > A list of sample correlation matrices.
   # - TList   > A list as SList of target matrices with the same structure as
-  #             SList.
+  #             SList. Default is given by default.target.
   # - ns      > A vector with the same length as SList giving the sample
   #             sizes.
   # - lambda1 > The ridge penalty (a postive number)
-  # - labmda2 > The fused penalty (a non-negative number)
+  # - lambda2 > The fused penalty (a non-negative number)
   # - max.ite > integer. The maximum number of interations, default is 100.
   # - verbose > logical. Should the function print extra info. Defaults to TRUE.
   # - eps     > numeric. A positive convergence criterion.
   ##############################################################################
 
   K <- length(SList)  # Number of groups
-  OList <- SList  # Initialize estimates
+  PList <- SList      # Initialize estimates
 
   if (verbose) {
-    cat("Iteration:  | Diff. in Omega in Frobenious norm for k = 1, ..., K\n")
+    cat("Iteration:  | Difference in Frobenious norm for k = ( 1 2 ... K )\n")
   }
   diffs <- rep(NA, K)
   for (i in seq_len(max.ite)) {
     for (k in seq_len(K)) {
-      tmpOmega <- .fusedUpdate(k0 = k, OList = OList, SList = SList,
+      tmpOmega <- .fusedUpdate(k0 = k, PList = PList, SList = SList,
                                TList = TList, ns = ns,
                                lambda1 = lambda1, lambda2 = lambda2)
 
@@ -115,8 +112,8 @@ fusedRidgeS <- function(SList, TList, ns, lambda1, lambda2,
         if (verbose) {cat(" ")}
       }
 
-      diffs[k] <- .FrobeniusLoss(tmpOmega, OList[[k]])/sum(tmpOmega^2)
-      OList[[k]] <- tmpOmega
+      diffs[k] <- .FrobeniusLoss(tmpOmega, PList[[k]])/sum(tmpOmega^2)
+      PList[[k]] <- tmpOmega
     }
     if (verbose) {
       cat(": i =", sprintf("%-2d", i), "| diffs = (", diffs, ")\n")
@@ -128,9 +125,8 @@ fusedRidgeS <- function(SList, TList, ns, lambda1, lambda2,
   if (i == max.ite) {
     warning("Maximum iterations (", max.ite, ") hit")
   }
-  return(OList)
+  return(PList)
 }
-
 
 
 
